f8f7e554615fc94f7ce02a1bf9b5579a133760f8
ParisZX <parad2000@hotmail.com>
2013-07-14 00:27:07 +0300

    finish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#Returns the full page title one a per-page basis
+	def full_title(page_title)
+		base_title = "Jynx"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
