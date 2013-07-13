706b2a61c1f6deb5692a346d8570189fc4663804
ParisZX <parad2000@hotmail.com>
2013-07-13 22:39:20 +0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectJynx::Application.config.secret_token = '69a8c3af5a2e9ebf9eea04f96639d47c8b24a3aaff673a3ba5e1cdd932939ee52c65daaae00a093bc8561f24ad4f73e6e2ed566b7bc8e57152a8e1783441d0b7'
