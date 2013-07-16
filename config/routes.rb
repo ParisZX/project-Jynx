11d9368a93f3769af1014c620a1db34f59d2e7ab
ParisZX <parad2000@hotmail.com>
2013-07-16 10:18:09 +0300

    create new routes, and a users controlller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ ProjectJynx::Application.routes.draw do
   get "users/new"
 
   root to: 'static_pages#home'
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
-
+  match '/signup',  to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
