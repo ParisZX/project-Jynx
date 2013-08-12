3e5a15b80cf00d3b2a609d838c19da258889e8cf
ParisZX <parad2000@hotmail.com>
2013-08-12 21:27:13 +0300

    fix problem with production db

--- db/migrate/20130804122150_create_messages.rb
+++ db/migrate/20130804122150_create_messages.rb
@@ -3,7 +3,7 @@ class CreateMessages < ActiveRecord::Migration
     create_table :messages do |t|
     	t.string :sender_id,:null => false
 		t.string :recepient_id
-		t.boolean :sender_deleted, :recepient_deleted, :default => 0
+		t.boolean :sender_deleted, :recepient_deleted, :default => false
 		t.text :body
 		t.datetime :read_at
 		t.string :container,:default => "draft"
